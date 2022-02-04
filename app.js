caa0b091193b776b1ec48b2feef65268e843ec5f
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-02-03 23:38:20 -0800

    handling 404 responses

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ const express = require("express");
 
 const connectDB = require("./db/connect");
 const tasksRoutes = require("./routes/tasks");
+const notFound = require("./middleware/not-found");
 
 const app = express();
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
 app.use("/api/v1/tasks", tasksRoutes);
-
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
+app.use(notFound);
 
 const port = 3000;
 
